ff45bcf1249da152e6c41b31e740c97e9fa79cd8
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2020-10-07 23:07:20 +0100

    Add skeleton code

---
+++ src/main/java/BinaryTree.java
@@ -0,0 +1,18 @@
+class TreeNode {
+
+  int value;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode(int value) {
+    this.value = value;
+  }
+}
+
+public class BinaryTree {
+  TreeNode root;
+
+  public TreeNode lowestCommonAncestor(TreeNode node1, TreeNode node2) {
+    return null;
+  }
+}
